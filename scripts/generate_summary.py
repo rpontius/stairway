be246ef20367277a969ced817cc556008153d639
rpont238 <ryanpontius@gmail.com>
2025-02-27 11:58:58 -0800

    Fixed a small oversight that swaps "Total_Mutations" with "Mutation_BP_Total" in the summary text file.

--- scripts/generate_summary.py
+++ scripts/generate_summary.py
@@ -7,21 +7,24 @@ import os
 def tabulate_mutations(files, output, file_name):
     #with open(output, 'w') as out:
     data_output = []
+    total_structural_variants = 0
     for file, name in zip(files, file_name):
         print(file)
         with gzip.open(file, 'rt') as f:
             df = pd.read_csv(f, delimiter='\t')
-            n = len(df) # total mutations
+            total_mutations = len(df) # total mutations
             mut_bp_total = df['SVLEN'].sum()
             mut_bp_mean = df['SVLEN'].mean()
             mut_bp_std = df['SVLEN'].std()
-            data_output.append([name, mut_bp_total, n, mut_bp_mean, mut_bp_std])
-    df_output = pd.DataFrame(data_output, columns=['Bed Type', 'Total Mutations', 'Total Mut BPs', 'Mean', 'Std Dev'])
+            data_output.append([name, total_mutations, mut_bp_total, mut_bp_mean, mut_bp_std])
+            if name in ["sv_del", "sv_ins", "sv_inv"]:
+                total_structural_variants += total_mutations
+    df_output = pd.DataFrame(data_output, columns=['Bed_Type', 'Total_Mutations', 'Mutation_BP_Count', 'Mean', 'Std_Dev'])
     df_output.to_csv(output, sep='\t', index=False)
 
-    return output
+    return output, total_structural_variants
 
-def hetsep_count(hetsep, output_file):
+def hetsep_count(hetsep, output_file, structural_variants):
     
     total_lines = 0
     for filename in os.listdir(hetsep):
@@ -34,6 +37,7 @@ def hetsep_count(hetsep, output_file):
         
     with open(output_file, 'a') as out:
         out.write(f"\nTotal lines in hetsep directory: {total_lines}\n")
+        out.write(f"#Structural Variants: {structural_variants}\n")
                 
 
 
@@ -52,8 +56,8 @@ def main():
     file_name = ["indel_del", "indel_ins", "snv_snv", "sv_del", "sv_ins", "sv_inv"]
 
 
-    output_file = tabulate_mutations(file_list, output, file_name)
-    hetsep_count(hetsep, output_file)
+    output_file, structural_variants = tabulate_mutations(file_list, output, file_name)
+    hetsep_count(hetsep, output_file, structural_variants)
 
 if __name__ == "__main__":
     main()
